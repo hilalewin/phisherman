bf19f57759137cb1044a6c948046c5cef2fda41e
hilalewin <hilalewin98@gmail.com>
2023-06-04 02:20:13 +0300

    Added the 'background part' of refactoring

--- content.js
+++ content.js
@@ -4,11 +4,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       const token = message.token;
       const legacyThreadId = document.querySelector('[role="main"] [data-legacy-thread-id]').getAttribute('data-legacy-thread-id');
       // Call your specific function with the token value
-      readMessage(legacyThreadId,token);
+      readMessageAndAnalyzeIfUnread(legacyThreadId,token);
     }
   });
   
-function readMessage(messageId, token) {
+function readMessageAndAnalyzeIfUnread(messageId, token) {
   const url = `https://www.googleapis.com/gmail/v1/users/me/messages/${messageId}`;
   fetch(url, {
     headers: {
@@ -24,88 +24,101 @@ function readMessage(messageId, token) {
       // Only if the message is unread
       // isUnread
       if (1){
-
-        // Extract the required data from the email
-        const payload = data.payload;
-        const headers = payload.headers.reduce((acc, header) => {
-          acc[header.name.toLowerCase()] = header.value;
-          return acc;
-        }, {});
-
-        const links = extractLinksFromSnippet(data.snippet);
-
-        // Encoded, will fix later
-        const body = payload.body.data;
-
-        // Create the payload object
-        const extractedData = {
-          subject: headers.subject,
-          time: headers.date,
-          senderEmail: headers.from, 
-          content: data.snippet,
-          encoded_content: body,
-          links: links 
-        };
-
-        /*
-        const fromHeader = data.payload.headers.find(header => header.name.toLowerCase() === 'from');
-        const senderValue = fromHeader ? fromHeader.value : '';
-        const senderMatches = senderValue.match(/(.*)<(.+@[^>]+)>/);
-
-        let senderName = '';
-        let senderEmail = '';
-
-        if (senderMatches) {
-            senderName = senderMatches[1].trim();
-            senderEmail = senderMatches[2].trim();
-        } else {
-            senderName = senderValue.trim();
+        
+        analyzeMessage(data);
         }
-        */
-      
-
-        fetch('http://localhost:5000/analyze', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(extractedData),
         })
-        .then(response => response.json())
-          .then(data => {
-            
-            alert(data['content']);
-            /*
-            alert(data['subject']);
-            alert(data['links']);
-            alert(data['content']);
-            
-            subject: headers.subject,
-            time: headers.date,
-            senderEmail: headers.from, 
-            content: data.snippet,
-            links: links
-            alert(labelIds)
-            */
-          })
-          .catch(error => {
+        .catch(error => {
               // Handle any errors
               console.log("error")
         });
         
-          
-    }
-
-  })
-  .catch(error => {
-      // Handle any errors that occurred during the API request
-      
-      console.error(error);
-      
-      // If the message was sent by the user, then the API call will return nothing.
-      // (As wanted)
-  });
 }
+
+function decodeMessageBody(encodedData) {
+    // TODO: decode data
+    return encodedData;
+}
+
+
+function createAnalyzeRequestPayload(data) {
+    // Extract the required data from the email
+    const headers = data.payload.headers.reduce((acc, header) => {
+      acc[header.name.toLowerCase()] = header.value;
+      return acc;
+    }, {});
+  
+    const links = extractLinksFromSnippet(data.snippet);
+  
+    const body = decodeMessageBody(data.payload.body.data);
+  
+    // Create the payload object
+    const extractedData = {
+      subject: headers.subject,
+      time: headers.date,
+      senderEmail: headers.from,
+      content: data.snippet,
+      encoded_content: body,
+      links: links
+    };
+  
+    return JSON.stringify(extractedData);
+  }
+
+
+function sendAnalyzeRequest(payload) {
+
+    fetch('http://localhost:5000/analyze', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: payload,
+      })
+        .then(response => response.json())
+        .then(data => {
+    
+          alert(data['content']);
+          /*
+          alert(data['subject']);
+          alert(data['links']);
+          alert(data['content']);
+          
+          subject: headers.subject,
+          time: headers.date,
+                senderEmail: headers.from, 
+                content: data.snippet,
+                links: links
+                alert(labelIds)
+                */
+        })
+        .catch(error => {
+          // Handle any errors
+          console.log("error")
+        });
+}
+
+function analyzeMessage(data) {
+
+    const payload = createAnalyzeRequestPayload(data);
+    /*
+    const fromHeader = data.payload.headers.find(header => header.name.toLowerCase() === 'from');
+    const senderValue = fromHeader ? fromHeader.value : '';
+    const senderMatches = senderValue.match(/(.*)<(.+@[^>]+)>/);
+
+    let senderName = '';
+    let senderEmail = '';
+
+    if (senderMatches) {
+        senderName = senderMatches[1].trim();
+        senderEmail = senderMatches[2].trim();
+    } else {
+        senderName = senderValue.trim();
+    }
+    */
+    sendAnalyzeRequest(payload);
+}
+
 
 function extractLinksFromSnippet(snippet) {
     const urlRegex = /(https?:\/\/[^\s]+)/g;
