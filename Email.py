71f0c202063b5778393a7dc338a36d3fd20a73fc
hilalewin <hilalewin98@gmail.com>
2023-06-18 23:22:29 +0300

    With prints

--- Email.py
+++ Email.py
@@ -17,3 +17,6 @@ class Email:
     def from_json(cls, json_str):
         data = json.loads(json_str)
         return cls(**data)
+    
+    def __str__(self):
+        return f"Sender Email: {self.sender_email}\nTime: {self.time}\nSubject: {self.subject}\nContent: {self.content}\nLinks: {self.links}\nDecoded Content: {self.decoded_content}\nCounter from Sender: {self.counter_from_sender}"
